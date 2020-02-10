161ad950d9288a2646d4d3645e2a7ae9d1da3ca8
Evgenii Stratonikov <kraunid@gmail.com>
2020-02-10 20:25:58 +0300

    rename element to elem
    It is much shorter to type.

--- tangram.m
+++ tangram.m
@@ -7,12 +7,12 @@
 :- implementation.
 :- import_module int, list, string.
 
-% element is discriminated union of 2 operations:
+% elem is discriminated union of 2 operations:
 %   step(a, b) -- is a step forward on a * x + b * y, where (x,y) is some basis.
 %     This is needed because x and y can be non-reductible to each other via rational number.
 %     In a classic tangram we have x = 1 and y = square root of 2 (or some multiple of it, actually 0.5 * sqrt(2)).
 %  turn(direction, degree) -- is a turn to direction for a specified number of degrees.
-:- type element ---> step(int,int); turn(int).
+:- type elem ---> step(int,int); turn(int).
 :- type direction ---> left; right.
 
 main(!IO) :-
@@ -45,22 +45,22 @@ main(!IO) :-
         io.write("no solutions", !IO)
     ).
 
-:- func left(int) = element.
+:- func left(int) = elem.
 left(A) = turn(A rem 360).
 
-:- func right(int) = element.
+:- func right(int) = elem.
 right(A) = turn((-A) rem 360).
 
-:- pred add_turns(element::in, element::in, element::out) is semidet.
+:- pred add_turns(elem::in, elem::in, elem::out) is semidet.
 add_turns(turn(Deg1),turn(Deg2), turn((Deg1 + Deg2) rem 360)).
 
-:- pred append_turns(element::in, element::in, element::out) is semidet.
+:- pred append_turns(elem::in, elem::in, elem::out) is semidet.
 append_turns(turn(Deg1), turn(Deg2), turn((Deg1+Deg2+180) rem 360)).
 
-:- pred add_steps(element::in, element::in, element::out) is semidet.
+:- pred add_steps(elem::in, elem::in, elem::out) is semidet.
 add_steps(step(A1,B1),step(A2,B2), step(A1+A2,B1+B2)).
 
-:- func normalize(list(element)) = list(element).
+:- func normalize(list(elem)) = list(elem).
 normalize([E1, E2 | Es]) = X :-
   (
     if add_turns(E1, E2, E)
@@ -74,31 +74,31 @@ normalize([E1, E2 | Es]) = X :-
 normalize([E]) = [E].
 normalize([]) = [].
 
-% Elements are combined when we put them next to each other.
+% elems are combined when we put them next to each other.
 % This means:
 %   1. Find a side (or `step`) in each of them.
 %   2. Collapse 2 lists into one.
 %   3. Possibly normalize the resulting list:
 %      - turn(0) and step(0,0) can be removed
 %      - 2 sucessive steps can be combined
-% :- pred combine_at(list(element)::in, int::in, list(element)::in, int::in, list(element)::out) is nondet.
+% :- pred combine_at(list(elem)::in, int::in, list(elem)::in, int::in, list(elem)::out) is nondet.
 % combine_at([E|Es],I1,F2,I2,F) = X :-
 %   if combine_at(Es,)
 
-:- pred sub_turns(element::in, element::in, element::out) is semidet.
+:- pred sub_turns(elem::in, elem::in, elem::out) is semidet.
 sub_turns(turn(Deg1), turn(Deg2), turn((Deg1-Deg2) rem 360)).
 
-:- pred sub_steps(element::in, element::in, element::out) is semidet.
+:- pred sub_steps(elem::in, elem::in, elem::out) is semidet.
 sub_steps(step(A1,B1), step(A2,B2), step(A1-A2,B1-B2)).
 
-:- pred invert_turn(element::in, element::out) is semidet.
+:- pred invert_turn(elem::in, elem::out) is semidet.
 invert_turn(turn(D), turn((-D) rem 360)).
 
 % insert_after inserts third argument between first and second.
 % first argument must end in turn
 % second argument must start from step
 % third argument must start in turn
-:- pred insert_after(list(element)::in, list(element)::in, list(element)::in, list(element)::out) is semidet.
+:- pred insert_after(list(elem)::in, list(elem)::in, list(elem)::in, list(elem)::out) is semidet.
 insert_after(Es1, [S2,T2|Es2], [E3|Es3], X) :-
   (
     if split_last(Es1, M1, L1),
